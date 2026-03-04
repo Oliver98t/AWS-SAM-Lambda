26f6611fda43c905f333e20a3a7878ee7bebe271
Oliver98t <oli1998t@gmail.com>
2026-03-04 19:20:39 +0000

    added sqs event

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,48 @@
+import boto3
+import os
 import json
 
-# import requests
+# define the DynamoDB table that Lambda will connect to
+tablename = os.environ['LAMBDAIACTABLE_TABLE_NAME']
 
+# create the DynamoDB resource
+dynamo = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+    print(f"event: {event} \ncontetx: {context}")
+    '''
+    # get the message out of the SQS event
+    message = event['Records'][0]['body']
+    data = json.loads(message)
+    # write event data to DDB table
+    if check_message_format(data):
+        key = next(iter(data))
+        value = data[key]
+        dynamo.put_item(
+            TableName=tablename,
+            Item={
+                'id': {'S': key},
+                'Value': {'S': value}
+            }
+        )
+    else:
+        raise ValueError("Input data not in the correct format")
+    #'''
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        'statusCode': 200,
+        'body': json.dumps('Message processed and stored in DynamoDB')
     }
+
+# check that the event object contains a single key value  
+# pair that can be written to the database
+def check_message_format(message):
+    if len(message) != 1:
+        return False
+        
+    key, value = next(iter(message.items()))
+    
+    if not (isinstance(key, str) and isinstance(value, str)):
+        return False
+
+    else:
+        return True
